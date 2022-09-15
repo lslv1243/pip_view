4ad01379ec1fbe7ff74a10e353bcafd044df27ff
MCYBA <36483874+MCYBA@users.noreply.github.com>
2022-09-15 08:58:07 +0300

    Add floatingBorderRadius member

--- lib/src/pip_view.dart
+++ lib/src/pip_view.dart
@@ -7,6 +7,7 @@ class PIPView extends StatefulWidget {
   final PIPViewCorner initialCorner;
   final double? floatingWidth;
   final double? floatingHeight;
+  final double? floatingBorderRadius;
   final bool avoidKeyboard;
 
   final Widget Function(
@@ -20,6 +21,7 @@ class PIPView extends StatefulWidget {
     this.initialCorner = PIPViewCorner.topRight,
     this.floatingWidth,
     this.floatingHeight,
+    this.floatingBorderRadius,
     this.avoidKeyboard = true,
   }) : super(key: key);
 
@@ -65,6 +67,7 @@ class PIPViewState extends State<PIPView> with TickerProviderStateMixin {
       ),
       floatingHeight: widget.floatingHeight,
       floatingWidth: widget.floatingWidth,
+      floatingBorderRadius: widget.floatingBorderRadius,
       initialCorner: widget.initialCorner,
     );
   }
